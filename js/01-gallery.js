f0df055509e9d35ea5bcd1f5da32a392e9f6392f
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2022-10-17 01:32:33 +0300

    add 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onGalleryPictureClick);
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+        })
+        .join('');
+};
+
+function onGalleryPictureClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  };
+  const galleryElement = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+	        <img
+              src="${galleryElement}"
+              width = "800"
+              height = "600"/>
+    `);
+  instance.show();
+  };
