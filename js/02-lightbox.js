bb170607b9645ce2c13cc95b12a8b8d2ac0700ac
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2022-10-17 22:22:08 +0300

    add 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onGalleryPictureClick);
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+        })
+        .join('');
+};
+
+function onGalleryPictureClick(event) {
+  event.preventDefault();
+      if (event.key === "Escape") {
+      modalWindow.close();
+    }
+    const lightbox = new SimpleLightbox('.gallery a', { disableRightClick: true, });
+};
+  
+
+    
+
+
+
+
